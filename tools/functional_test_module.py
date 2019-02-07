14d0cd29b3339c52b39c9e6fafb9541469c87ae4
Noel Bundick <noelbundick@gmail.com>
2019-02-07 13:47:55 -0800

    Format functional_test_module w/ autopep8

--- tools/functional_test_module.py
+++ tools/functional_test_module.py
@@ -19,34 +19,36 @@ class FunctionalTestModule(ModuleBase):
     def __init__(self, subscription_id):
         self.subscription_id = subscription_id
         self.credentials = self.get_credentials()
-        self.client = ResourceManagementClient(self.credentials, self.subscription_id)
+        self.client = ResourceManagementClient(
+            self.credentials, self.subscription_id)
         self.policy = PolicyModule(self.subscription_id)
 
     def run(self, policy_path):
         # example policy_path: 'definitions/Microsoft.Storage/deny-unrestricted-access.json'
         policy_definition_name = os.path.basename(policy_path)[:-5]
-        
+
         # Create a RG w/ policy definition & assignments
         resource_group = policy_definition_name
         self._setup(resource_group, policy_path)
-        policy_definition_id = '/subscriptions/{}/providers/Microsoft.Authorization/policyDefinitions/{}'.format(self.subscription_id, policy_definition_name)
+        policy_definition_id = '/subscriptions/{}/providers/Microsoft.Authorization/policyDefinitions/{}'.format(
+            self.subscription_id, policy_definition_name)
 
         # Create deployments to exercise policies
         positive_tests, negative_tests = self._discover_tests(policy_path)
-        self._run_negative_tests(resource_group, negative_tests, policy_definition_id)
+        self._run_negative_tests(
+            resource_group, negative_tests, policy_definition_id)
         self._run_positive_tests(resource_group, positive_tests)
 
         # Teardown RG
         self._teardown(resource_group)
         # TODO: Delete policy definition
 
-
     def _setup(self, resource_group, policy_path):
         print('Creating resource group: ', resource_group)
-        self.client.resource_groups.create_or_update(resource_group, {'location': 'westus2'})
+        self.client.resource_groups.create_or_update(
+            resource_group, {'location': 'westus2'})
         self.policy.deploy(policy_path, None)
         # TODO: Create a policy assignment
-
 
     def _discover_tests(self, policy_path):
         # expected: ['definitions', 'Microsoft.Storage', 'deny-unrestricted-access.json']
@@ -75,11 +77,9 @@ class FunctionalTestModule(ModuleBase):
 
         return positive_tests, negative_tests
 
-
     def _teardown(self, resource_group):
         print('Deleting resource group: ', resource_group)
         self.client.resource_groups.delete(resource_group)
-
 
     def _run_negative_tests(self, resource_group, negative_tests, policy_definition_id):
         for template_path in negative_tests:
@@ -91,10 +91,10 @@ class FunctionalTestModule(ModuleBase):
                 print('Error: Non-compliant template was successfully deployed')
             else:
                 if self._is_failed_due_to_policy(ex, policy_definition_id):
-                    print('Pass: Bad template blocked by policy definition: ', policy_definition_id)
+                    print('Pass: Bad template blocked by policy definition: ',
+                          policy_definition_id)
                 else:
                     print('Error: Deployment failed with error: ', ex)
-
 
     def _run_positive_tests(self, resource_group, positive_tests):
         for template_path in positive_tests:
@@ -107,7 +107,6 @@ class FunctionalTestModule(ModuleBase):
             else:
                 print('Error: Deployment failed with error: ', ex)
 
-
     def _deploy_template(self, resource_group, template_path):
         template_name = os.path.basename(template_path)
         print('Deploying template: ', template_name)
@@ -119,22 +118,22 @@ class FunctionalTestModule(ModuleBase):
                 'mode': DeploymentMode.incremental,
                 'template': template
             }
-            deployment = self.client.deployments.create_or_update(resource_group, template_name, properties)
+            deployment = self.client.deployments.create_or_update(
+                resource_group, template_name, properties)
             deployment.wait()
             return DeploymentResult.Success, None
         except Exception as ex:
             return DeploymentResult.DeploymentFailure, ex
 
-
     def _is_failed_due_to_policy(self, ex, policy_definition_id):
         if type(ex) is not CloudError:
             return False
-        
+
         text = ex.error.response.text
         err = json.loads(text)['error']
 
         # This works for the trivial case but may fail if there are additional 'details' or 'additionalInfo' elements
-        return (err['code'] == 'InvalidTemplateDeployment' 
-            and err['details'][0]['code'] == 'RequestDisallowedByPolicy' 
-            and err['details'][0]['additionalInfo'][0]['type'] == 'PolicyViolation'
-            and err['details'][0]['additionalInfo'][0]['info']['policyDefinitionId'] == policy_definition_id)
+        return (err['code'] == 'InvalidTemplateDeployment'
+                and err['details'][0]['code'] == 'RequestDisallowedByPolicy'
+                and err['details'][0]['additionalInfo'][0]['type'] == 'PolicyViolation'
+                and err['details'][0]['additionalInfo'][0]['info']['policyDefinitionId'] == policy_definition_id)
